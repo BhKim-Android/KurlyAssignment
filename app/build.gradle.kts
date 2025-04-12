967cc8a762d75a589df7c432dd38c0bb1901561e
kimbh <kimbh@wemakeprice.com>
2025-04-13 00:52:20 +0900

    kapt 적용 ViewModel, 코루틴, paging, hilt 적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("org.jetbrains.kotlin.kapt")
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -63,4 +65,18 @@ dependencies {
     implementation(libs.okhttp)
     implementation(libs.gson)
     implementation(libs.adapter.coroutines)
+
+    implementation(libs.lifecycle.viewmodel.ktx)
+
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.paging.runtime)
+    implementation(libs.androidx.paging.compose)
+
+    implementation(libs.hilt.android)
+    kapt(libs.dagger.hilt.android.compiler)
+}
+
+kapt {
+    correctErrorTypes = true
 }
