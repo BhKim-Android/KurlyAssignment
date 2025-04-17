13b511278da623b0bc57a6e811741441c66f524f
kimbh <kimbh@wemakeprice.com>
2025-04-18 03:28:03 +0900

    당겨서 새로 고침 추가.

--- app/src/main/java/com/kurly/kurlyassignment/MainActivity.kt
+++ app/src/main/java/com/kurly/kurlyassignment/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -36,40 +43,57 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Sections(viewmodel: MainViewmodel = hiltViewModel()) {
+    var isRefreshing by remember { mutableStateOf(false) }
     val lazyPagingItems = viewmodel.sectionsWithProductsFlow.collectAsLazyPagingItems()
-    val brush = rememberShimmerBrush()
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(20.dp)) {
-        if (lazyPagingItems.loadState.refresh is LoadState.Loading) {
-            items(5) {
-                SectionPlaceHolder(brush)
-                HorizontalPlaceHolder(brush)
-                VerticalSectionPlaceHolder(brush)
-            }
-        } else {
-            items(
-                lazyPagingItems.itemCount,
-                key = lazyPagingItems.itemKey { it.section.id }
-            ) { index ->
-                lazyPagingItems[index]?.let { sectionWithProduct ->
-                    SectionContent(sectionWithProduct)
-                }
 
-                if (index < lazyPagingItems.itemCount - 1) {
-                    HorizontalDivider(
-                        thickness = 1.dp,
-                        color = Color.Gray
-                    )
-                }
-            }
+    LaunchedEffect(lazyPagingItems.loadState.refresh) {
+        if (isRefreshing && lazyPagingItems.loadState.refresh !is LoadState.Loading) {
+            isRefreshing = false
         }
+    }
 
-        if (lazyPagingItems.loadState.append is LoadState.Loading) {
-            item {
-                SectionPlaceHolder(brush)
-                HorizontalPlaceHolder(brush)
-                VerticalSectionPlaceHolder(brush)
+    val brush = rememberShimmerBrush()
+    PullToRefreshBox(
+        isRefreshing = isRefreshing,
+        onRefresh = {
+            isRefreshing = true
+            lazyPagingItems.refresh()
+        }
+    ) {
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(20.dp)) {
+            if (lazyPagingItems.loadState.refresh is LoadState.Loading) {
+                items(5) {
+                    SectionPlaceHolder(brush)
+                    HorizontalPlaceHolder(brush)
+                    VerticalSectionPlaceHolder(brush)
+                }
+            } else {
+                items(
+                    lazyPagingItems.itemCount,
+                    key = lazyPagingItems.itemKey { it.section.id }
+                ) { index ->
+                    lazyPagingItems[index]?.let { sectionWithProduct ->
+                        SectionContent(sectionWithProduct)
+                    }
+
+                    if (index < lazyPagingItems.itemCount - 1) {
+                        HorizontalDivider(
+                            thickness = 1.dp,
+                            color = Color.Gray
+                        )
+                    }
+                }
+            }
+
+            if (lazyPagingItems.loadState.append is LoadState.Loading) {
+                item {
+                    SectionPlaceHolder(brush)
+                    HorizontalPlaceHolder(brush)
+                    VerticalSectionPlaceHolder(brush)
+                }
             }
         }
     }
